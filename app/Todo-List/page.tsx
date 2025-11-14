1abab779e6d801149500982fa69f478a0a2768a2
Shishi <eonsirimool74@gmail.com>
2025-11-14 17:46:34 +0700

    first commit

--- app/Todo-List/page.tsx
+++ app/Todo-List/page.tsx
@@ -39,9 +39,10 @@ export default function TodoListpage() {
     };
 
     const handleDelete = (id: number) => {
-    if (window.confirm("Delete this task?")) {
-        setTodos(todos.filter(todo => todo.id !== id));
-    }    };
+        if (window.confirm("Delete this task?")) {
+            setTodos(todos.filter(todo => todo.id !== id));
+        }
+    };
 
     useEffect(() => {
         const stored = localStorage.getItem("todos");
@@ -57,56 +58,42 @@ export default function TodoListpage() {
     return (
         <div className="wrapper mx-auto my-20 rounded-3xl border-2 p-10 w-[680px] border-gray ">
             <h1 className="text-center text-3xl">To-do List</h1>
+
             {/* ADD */}
             <div className="Adding flex justify-center items-center my-5">
                 <input className="px-5 py-2 rounded-xl w-[70%] mr-2 border border-gray-500 focus:border-blue-500" type="text" placeholder="Add a new task" value={inputText} onChange={(e) => setInput(e.target.value)} />
                 <button
                     className="px-4 py-2 rounded-xl cursor-pointer border border-gray-800 bg-slate-900 text-white text-sm sm:text-base font-medium flex items-center justify-center gap-1 shadow-md hover:bg-slate-800 active:scale-[0.98] transition"
-                    onClick={handleAdd}
-                >
+                    onClick={handleAdd}>
                     <Plus className="w-4 h-4" />
                     <span className="hidden sm:inline">Add</span>
                 </button>
             </div>
+
             {/* filters */}
             <div className="controls m-3 ">
                 <div className="filters m-3 flex justify-center items-center gap-2">
-
-                    <button
-                        className={`cursor-pointer px-3 py-1 rounded-full border 
-                ${filter === "all"
-                                ? "bg-black text-white border-black"
-                                : "text-gray-600 border-gray-300 hover:bg-gray-100"}
-            `}
-                        onClick={() => setFilter("all")}
-                    >
+                    <button className={`cursor-pointer px-3 py-1 rounded-full border 
+                                     ${filter === "all" ? "bg-black text-white border-black" : "text-gray-600 border-gray-300 hover:bg-gray-100"}
+                                    `}
+                        onClick={() => setFilter("all")}>
                         All
                     </button>
-
-                    <button
-                        className={`cursor-pointer px-3 py-1 rounded-full border 
-                ${filter === "pending"
-                                ? "bg-black text-white border-black"
-                                : "text-gray-600 border-gray-300 hover:bg-gray-100"}
-            `}
-                        onClick={() => setFilter("pending")}
-                    >
+                    <button className={`cursor-pointer px-3 py-1 rounded-full border 
+                                     ${filter === "all" ? "bg-black text-white border-black" : "text-gray-600 border-gray-300 hover:bg-gray-100"}
+                                    `}
+                        onClick={() => setFilter("pending")}>
                         Pending
                     </button>
-
-                    <button
-                        className={`cursor-pointer px-3 py-1 rounded-full border 
-                ${filter === "completed"
-                                ? "bg-black text-white border-black"
-                                : "text-gray-600 border-gray-300 hover:bg-gray-100"}
-            `}
-                        onClick={() => setFilter("completed")}
-                    >
+                    <button className={`cursor-pointer px-3 py-1 rounded-full border 
+                                     ${filter === "all" ? "bg-black text-white border-black" : "text-gray-600 border-gray-300 hover:bg-gray-100"}
+                                    `}
+                        onClick={() => setFilter("completed")}>
                         Completed
                     </button>
-
                 </div>
             </div>
+
             {/* Lists */}
             <ul className="todos flex flex-col gap-3 ">
                 {filteredTodos.map((todo) => (
@@ -117,29 +104,24 @@ export default function TodoListpage() {
                                 textDecoration:
                                     todo.status === "completed" ? "line-through" : "none",
                                 opacity: todo.status === "completed" ? 0.5 : 1,
-                            }}
-                        >
+                            }}>
                             {todo.text}
                         </span>
                         <span className="buttons float-right flex ">
                             <button
                                 className="px-2 py-1 mr-2 rounded-lg cursor-pointer border border-gray-800 bg-slate-900 text-white text-sm sm:text-base  flex items-center justify-center gap-1  hover:bg-slate-800 "
-                                onClick={() => handleComplete(todo.id)}
-                            >
+                                onClick={() => handleComplete(todo.id)}>
                                 <Check className="w-4 h-4" />
                                 <span className="hidden sm:inline text-[16px]">Completed</span>
                             </button>
                             <button
                                 className="px-2 py-1  rounded-lg cursor-pointer border border-gray-800 bg-white text-gray-800 text-sm sm:text-base  flex items-center justify-center gap-1  hover:bg-slate-100"
-                                onClick={() => handleDelete(todo.id)}
-                            >
+                                onClick={() => handleDelete(todo.id)}>
                                 <span>X</span>
                                 <span className="hidden sm:inline text-[16px]">Delete</span>
                             </button>
                         </span>
                     </li>
-
-
                 ))}
 
                 {filteredTodos.length === 0 && (
